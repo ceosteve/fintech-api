86efc7019b71621c5d3c29fac4b80fd252e43dfa
ceosteve <steven01njoroge@gmail.com>
2025-11-07 15:51:41 +0300

    function to create refresh tokens and persist in the database

--- app/authentication/oauth2.py
+++ app/authentication/oauth2.py
@@ -9,7 +9,7 @@ from app.schemas import tokens_schemas
 from sqlalchemy.orm import Session
 from fastapi import Depends
 
-ALGORYTHM= settings.algorithm
+ALGORITHM= settings.algorithm
 EXPIRATION_TIME= settings.access_token_expiration_time
 SECRET_KEY = settings.secret_key
 
@@ -19,13 +19,13 @@ def create_access_token(data:dict):
     expires = datetime.utcnow() + timedelta(minutes=EXPIRATION_TIME)
     to_encode.update({"exp":expires})
 
-    encoded_jwt=jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORYTHM)
+    encoded_jwt=jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
     return encoded_jwt
 
 
 def verify_access_token(token:str, credentials_exception):
     try:
-        payload= jwt.decode(token, SECRET_KEY, algorithms=[ALGORYTHM])
+        payload= jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         id:str = payload.get("user_id")
 
         if id is None:
@@ -44,11 +44,14 @@ def generate_raw_refresh_token() -> str:
 def make_refresh_record(user_id:str, db:Session=Depends(get_db), days:int=settings.refresh_token_expiration_days):
     raw_token=generate_raw_refresh_token()
     hashed_token=hash_token(raw_token)
-    expires= datetime.utcnow() + timedelta(days=days)
-    db_token= models.RefreshTokens(user_id=user_id, hashed_token=hashed_token, expires_at=expires)
+    expires_at = datetime.utcnow() + timedelta(days=days)
+    db_token = models.RefreshTokens(user_id=user_id, expires_at=expires_at, hashed_token=hashed_token)
+
     db.add(db_token)
     db.commit()
     db.refresh(db_token)
 
     return raw_token
+
+    
 
