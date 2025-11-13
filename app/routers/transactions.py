8b3516b33cdab7e1440334b58c3a2ecd3c4180a1
ceosteve <steven01njoroge@gmail.com>
2025-11-13 19:14:33 +0300

    combined db queries in the transfer endpoint and included ledger entries in deposit endpoint

--- app/routers/transactions.py
+++ app/routers/transactions.py
@@ -1,4 +1,5 @@
 
+
 from fastapi import Depends, HTTPException, status, APIRouter
 from app.database.database import get_db
 from app.dependencies import get_current_user
@@ -11,22 +12,26 @@ router = APIRouter(
     tags=["Transactions"]
 )
 
+
+"""deposit endpoint"""
 @router.post("/deposit", status_code=status.HTTP_200_OK, response_model=transaction_schemas.DepositResponse)
 def deposit_to_wallet(deposit_info:transaction_schemas.Deposit, db:Session=Depends(get_db),
                       current_user:str=Depends(get_current_user)):
     
-    existing_wallet = db.query(models.Wallets).filter(models.Wallets.user_id==current_user.id).first()
-    if not existing_wallet:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user has no digital wallet!")
-    
-    existing_account= db.query(models.Wallets).filter(models.Wallets.account_number==deposit_info.account_number).first()
+    existing_account = db.query(models.Wallets).filter(models.Wallets.user_id==current_user.id, 
+                                                      models.Wallets.account_number==deposit_info.account_number).first()
 
     if not existing_account:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"account number: {deposit_info.account_number} does not exist!")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"account number: {deposit_info.account_number} does not exist!")
     
     if existing_account.currency != deposit_info.currency:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
                             detail="You cannot deposit to an account with non-matching currency!")
+    
+    if existing_account.is_active =="False":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                             detail="You cannot transact with an inactive account!")
     
     existing_account.balance += deposit_info.amount
 
@@ -38,27 +43,41 @@ def deposit_to_wallet(deposit_info:transaction_schemas.Deposit, db:Session=Depen
         currency = deposit_info.currency,
         status = "completed"
     )
-    
     db.add(new_transaction)
+    db.flush()
+
+    new_ledger_entry = models.LedgerEntries(
+        transaction_id = new_transaction.id,
+        wallet_id = existing_account.id,
+        entry_type = models.EntryType.credit,
+        amount = deposit_info.amount,
+        balance_after =existing_account.balance,
+        currency = deposit_info.currency,
+        narration = "cash deposit"
+    )
+    
+    db.add(new_ledger_entry)
+
+
     db.commit()
     db.refresh(new_transaction)
+    db.refresh(new_ledger_entry)
 
     return {"message":f"You have successfully deposited {deposit_info.currency} {deposit_info.amount},new balance is {deposit_info.currency}:{existing_account.balance}"}
 
 
 
+"""withdrawal endpoint """
 @router.post("/withdraw", status_code=status.HTTP_200_OK, response_model=transaction_schemas.WithdrawalResponse)
 def withdraw_from_wallet(withdrawal_info:transaction_schemas.Withdrawal, 
                           db:Session=Depends(get_db), current_user:str=Depends(get_current_user)):
     
-    existing_wallet = db.query(models.Wallets).filter(models.Wallets.user_id==current_user.id).first()
-    if not existing_wallet:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user has no digital wallet!")
+    existing_account = db.query(models.Wallets).filter(models.Wallets.user_id==current_user.id,
+                                                      models.Wallets.account_number==withdrawal_info).first()
     
-    existing_account= db.query(models.Wallets).filter(models.Wallets.account_number==withdrawal_info.account_number).first()
-
     if not existing_account:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f" account number: {withdrawal_info.account_number} does not exist!")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f" account number: {withdrawal_info.account_number} does not exist!")
     
     if existing_account.balance < withdrawal_info.amount:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Insufficient balance")
@@ -67,6 +86,10 @@ def withdraw_from_wallet(withdrawal_info:transaction_schemas.Withdrawal,
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
                             detail="You cannot withdraw from an account with non-matching currency!")
     
+    if existing_account.is_active =="False":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                             detail="You cannot transact with an inactive account!")
+
     existing_account.balance -=withdrawal_info.amount
 
     new_transaction= models.Transactions(
@@ -87,6 +110,7 @@ def withdraw_from_wallet(withdrawal_info:transaction_schemas.Withdrawal,
 
 
 
+"""money transfer endpoint"""
 @router.post("/transfer", status_code=status.HTTP_200_OK, response_model=transaction_schemas.TransferOut)
 def transfer_to_wallet(transfer_info:transaction_schemas.Transer, db:Session=Depends(get_db),
                         current_user:str=Depends(get_current_user)):
@@ -98,21 +122,40 @@ def transfer_to_wallet(transfer_info:transaction_schemas.Transer, db:Session=Dep
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail="user has no digital wallet!")
     
-    sender_existing_account= db.query(models.Wallets).filter(
-        models.Wallets.account_number==transfer_info.sender_wallet_account_number).first()
+    """query for sender and receiver account numbers"""
+
+    accounts = db.query(models.Wallets).filter(models.Wallets.account_number.in_([
+        transfer_info.sender_wallet_account_number,
+        transfer_info.receiver_wallet_account_number
+    ])).all()
+
+
+    if len(accounts) !=2:
+        missing = {
+            transfer_info.sender_wallet_account_number,
+            transfer_info.receiver_wallet_account_number
+        } - {a.account_number for a in accounts}
+
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"wallet(s) not found for account(s):{','.join(map(str, missing))}")
+    
+    sender_existing_account = next(a for a in accounts if a.account_number==transfer_info.sender_wallet_account_number)
+    receiver_existing_account = next(a for a in accounts if a.account_number==transfer_info.receiver_wallet_account_number)
+
 
     if not sender_existing_account:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f" account number: {transfer_info.sender_wallet_account_number} does not exist!")
     
-    receiver_existing_account = db.query(models.Wallets).filter(
-        models.Wallets.account_number==transfer_info.receiver_wallet_account_number).first()
-    
     if  not receiver_existing_account:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f" account number: {transfer_info.receiver_wallet_account_number} does not exist")
     
-    if sender_existing_account.currency != transfer_info.currency and receiver_existing_account != transfer_info.currency:
+    if sender_existing_account.is_active==False or receiver_existing_account.is_active== False:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                             detail="You cannot transact with an inactive account!")
+    
+    if sender_existing_account.currency != transfer_info.currency and receiver_existing_account.currency != transfer_info.currency:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
                             detail="You cannot transact with non-matching currencies!")
     
